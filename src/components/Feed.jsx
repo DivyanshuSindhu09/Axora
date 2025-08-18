98b43ff8033ce4fa5a853d8400dd6baa89be918b
Divyanshu Sindhu <divyanshu.sindhu@gmail.com>
2025-08-18 10:21:08 +0530

    done

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -10,29 +10,32 @@ import toast from 'react-hot-toast'
 
 const Feed = () => {
 
+  //! dusro ki feed khali hogi 
+  
   const [feeds, setFeeds] = useState([])
   const [loading, setLoading] = useState(false)
 
-  const { getToken } = useAuth()
+  const {getToken} = useAuth()
 
   const fetchFeeds = async () => {
     const token = await getToken()
 
     try {
+
       setLoading(true)
 
-      const { data } = await api.get('/api/post/feed', {
-        headers: {
-          Authorization: `Bearer ${token}`
+      const {data} = await api.get('/api/post/feed', {
+        headers : {
+          Authorization : `Bearer ${token}`
         }
       })
 
-      if (data.success) {
+      if(data.success){
         setFeeds(data.posts)
-      } else {
+      }else{
         toast.error(data.message)
       }
-
+        
     } catch (error) {
       toast.error(error.message)
     }
@@ -45,17 +48,17 @@ const Feed = () => {
   }, [])
 
   return feeds.length > 0 ? (
-    <section className="max-h-screen text-white overflow-x-hidden overflow-y-scroll no-scrollbar flex flex-col lg:flex-row items-start justify-center lg:gap-8 bg-[#0f172a]">
-
+    <section className="max-h-screen text-white overflow-hidden overflow-y-scroll no-scrollbar flex flex-col lg:flex-row items-start justify-center lg:gap-8 bg-[#0f172a]">
+      
       {/* Stories + Post list */}
-      <div className="min-h-screen w-full lg:w-[70%] space-y-4">
+      <div className="min-h-screen w-screen lg:w-[70%] space-y-4">
         <div className="px-4 pt-4">
           <Stories />
         </div>
 
         <div className="p-4 no-scrollbar space-y-6">
           {feeds.map((post) => (
-            <div
+            <div 
               key={post._id}
               className="transition-transform duration-200 hover:scale-[1.01]"
             >
@@ -67,7 +70,7 @@ const Feed = () => {
 
       {/* Right Sidebar */}
       <div className="w-full lg:w-[25%] lg:block hidden sticky top-0 min-h-full space-y-6 pt-4">
-
+        
         {/* Sponsored Card */}
         <div className="bg-[#1e293b] text-sm font-[absans] p-4 rounded-xl flex flex-col gap-3 shadow-lg border border-[#334155] hover:shadow-[0_0_15px_rgba(251,191,36,0.4)] transition-all duration-300">
           <h3 className="text-[#fbbf24] font-semibold tracking-wide">Sponsored</h3>
